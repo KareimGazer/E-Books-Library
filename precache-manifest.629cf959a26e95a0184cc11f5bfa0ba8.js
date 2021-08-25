c9e49bb12604e3374f394480b40208f2729ba9c3
Kareim Tarek AbdelAzeem <kareimgazar1990@gmail.com>
2021-08-25 22:49:39 +0200

    Updates

---
+++ precache-manifest.629cf959a26e95a0184cc11f5bfa0ba8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ddb41f19afe200f31507",
+    "url": "/E-Books-Library/static/css/main.1c0c8986.chunk.css"
+  },
+  {
+    "revision": "ddb41f19afe200f31507",
+    "url": "/E-Books-Library/static/js/main.ddb41f19.chunk.js"
+  },
+  {
+    "revision": "d95b3ec45246991adbb8",
+    "url": "/E-Books-Library/static/js/1.d95b3ec4.chunk.js"
+  },
+  {
+    "revision": "476ba82d63f68999bb5e",
+    "url": "/E-Books-Library/static/js/runtime~main.476ba82d.js"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/E-Books-Library/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/E-Books-Library/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "/E-Books-Library/static/media/arrow-drop-down.9d4e3f36.svg"
+  },
+  {
+    "revision": "44e189c247850055c7dd8c9456f14314",
+    "url": "/E-Books-Library/index.html"
+  }
+];
